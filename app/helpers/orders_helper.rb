66c0b15df17fdef9fe47c30e37168742e8420490
Eduardo Rocha <eduardo.vinicius.rocha@gmail.com>
2019-06-20 18:53:02 -0300

    Order Helper

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,8 @@
 module OrdersHelper
+
+  def show_order_status(status)
+    order_status = OrderStatus.statuses.find {|key, value| value == status}.first
+    return order_status.humanize
+  end
+
 end
