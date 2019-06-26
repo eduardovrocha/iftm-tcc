ebeefd8a792d2f1c72c4688b3057d49aa55b0825
Eduardo Rocha <eduardo.vinicius.rocha@gmail.com>
2019-06-26 17:29:31 -0300

    Configure Views for Devise

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -1,3 +1,24 @@
 class Devise::SessionsController < DeviseController
 
+  # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
+
+  protected
+
+  def sign_in_params
+    devise_parameter_sanitizer.sanitize(:sign_in)
+  end
+
+  def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    {methods: methods, only: [:password]}
+  end
+
 end
