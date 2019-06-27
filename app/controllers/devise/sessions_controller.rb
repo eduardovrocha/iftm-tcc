e4c9684e7946cc81d622ce4c77ac56db9c4bdff3
Eduardo Rocha <eduardo.vinicius.rocha@gmail.com>
2019-06-27 16:31:47 -0300

    Confiugre login

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -1,3 +1,45 @@
 class Devise::SessionsController < DeviseController
 
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action(only: [:create]) {request.env["devise.skip_timeout"] = true}
+
+  # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
+
+  # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate(auth_options)
+    if self.resource
+      set_flash_message(:notice, :signed_in) if is_flashing_format?
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      redirect_to '/shop'
+    else
+      redirect_back(fallback_location: root_path, notice: 'Sorry but, we cant validate your access')
+    end
+  end
+
+  protected
+
+  def sign_in_params
+    devise_parameter_sanitizer.sanitize(:sign_in)
+  end
+
+  def serialize_options(resource)
+    methods = resource_class.authentication_keys.dup
+    methods = methods.keys if methods.is_a?(Hash)
+    methods << :password if resource.respond_to?(:password)
+    {methods: methods, only: [:password]}
+  end
+
+  def auth_options
+    {scope: resource_name, recall: "#{controller_path}#new"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,28 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-      sessions: 'devise/sessions',
-      registrations: 'devise/registrations',
-      passwords: 'devise/passwords'
-  }
-
   root :to => 'shop/products#index'
 
   namespace :admin do
-
     root :to => 'products#index'
-
     resources :order_items
     resources :orders
     resources :users
     resources :products
     resources :categories
-
   end
 
   namespace :shop do
-
     root :to => 'products#index'
-
     get '/products', to: 'products#index'
     get '/products/:id', to: 'products#show', as: 'product_detail'
     get '/cart', to: 'carts#show', as: 'cart'
-
   end
+
+  devise_for :users, controllers: {
+      sessions: 'devise/sessions',
+      registrations: 'devise/registrations',
+      passwords: 'devise/passwords'
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
